7e8239daca13b9ed6bcb49bb661de70cbc8507d0
Jaechan <dlwocks31@gmail.com>
2023-01-28 01:54:36 +0900

    사진과 옵션을 같이 내려주는 엔드포인트에서 몇 명이 맞췄는지도 보내주기

--- web/src/server/api/routers/question.ts
+++ web/src/server/api/routers/question.ts
@@ -1,3 +1,4 @@
+import { groupBy } from "lodash";
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const questionRouter = createTRPCRouter({
@@ -5,13 +6,20 @@ export const questionRouter = createTRPCRouter({
     const question = await ctx.prisma.question.findFirstOrThrow({
       include: {
         options: true,
+        submissions: true,
       },
     });
+
+    const submisssionsGrouped = groupBy(question.submissions, "optionId");
 
     const result = {
       id: question.id,
       url: question.url,
-      options: question.options.map((option) => option.text),
+      options: question.options.map((option) => ({
+        id: option.id,
+        text: option.text,
+        submitCount: submisssionsGrouped[option.id]?.length || 0,
+      })),
     };
     return result;
   }),
