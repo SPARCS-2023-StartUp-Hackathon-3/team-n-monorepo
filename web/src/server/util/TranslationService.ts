2e12671230817fd839206cb07d27aa0dfaeeee9b
Jaechan <dlwocks31@gmail.com>
2023-01-27 23:55:49 +0900

    타입 오류 수정

--- web/src/server/util/TranslationService.ts
+++ web/src/server/util/TranslationService.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { z } from "zod";
 
 export class TranslationService {
   private instance = axios.create({
@@ -6,17 +7,30 @@ export class TranslationService {
   });
 
   async enToKr(en: string): Promise<string> {
-    const { data } = await this.instance.post(
-      "/v1/papago/n2mt",
-      `source=en&target=ko&text=${en}`,
-      {
+    const data = await this.instance
+      .post<unknown>("/v1/papago/n2mt", `source=en&target=ko&text=${en}`, {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
           "X-Naver-Client-Id": "0ORpsoEpzmUBFto8osQy",
           "X-Naver-Client-Secret": "J0wJ5ok4f3",
         },
-      }
-    );
-    return data.message.result.translatedText;
+      })
+      .then((res) => res.data);
+    console.log("translate data", data);
+    const {
+      message: {
+        result: { translatedText },
+      },
+    } = z
+      .object({
+        message: z.object({
+          result: z.object({
+            translatedText: z.string(),
+          }),
+        }),
+      })
+      .parse(data);
+
+    return translatedText;
   }
 }
