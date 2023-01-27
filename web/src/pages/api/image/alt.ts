1dea70cd48c1528c7eaa13239f586f02e406f287
Jaechan <dlwocks31@gmail.com>
2023-01-27 21:33:00 +0900

    cors 이슈 해결

--- web/src/pages/api/image/alt.ts
+++ web/src/pages/api/image/alt.ts
@@ -1,7 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import NextCors from "nextjs-cors";
 import { z } from "zod";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const body = z
     .object({
       urls: z.array(z.string()),
