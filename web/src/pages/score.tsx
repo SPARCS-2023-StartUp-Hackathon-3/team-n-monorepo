daa5f3a26cfec7d2d13a6b8233e64128d80782cd
jyp930 <jyp930@gmail.com>
2023-01-29 05:12:03 +0900

    점수 페이지 CSS 개선

--- web/src/pages/score.tsx
+++ web/src/pages/score.tsx
@@ -24,19 +24,19 @@ const Score: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <div className="area">
+        <div className="area">
           <ul className="circles">
             <li></li>
             <li></li>
             <li></li>
           </ul>
-        <p className="p1">
-          {nickname} 님, 총 {me?.score}벌
-        </p>
-        <p className="p2">
-          {ranking?.length}명 중에 {me?.rank}등!
-        </p>
-        <div className="imgAlign">
+          <p className="p1">
+            {nickname}님, 총 {me?.score}벌
+          </p>
+          <p className="p2">
+            {ranking?.length}명 중에 {me?.rank}등!
+          </p>
+          <div className="imgAlign">
             <Image
               priority
               src="/logo_wink.gif"
@@ -44,31 +44,46 @@ const Score: NextPage = () => {
               width={449}
               height={300}
             />
+          </div>
+          <div className="subTitle">
+            <Image
+              priority
+              src="/sublogo.png"
+              alt="for Fashion"
+              width={207}
+              height={99}
+            />
+          </div>
+          <p className="p3">
+            {nickname}님 덕분에 NooN이 패션을 더 잘 들려줄 수 있게 되었어요
+            <br></br>감사해요 :)
+          </p>
+          <div className="bottonWrapper">
+            <button onClick={retry}>다시 시작하기</button>
+          </div>
         </div>
-        <p className="p3">
-          {nickname}님 덕분에 눈 NooN 이 패션을 더 잘 들려줄 수 있게 되었어요
-          <br></br>감사해요 :)
-        </p>
-        <div className="bottonWrapper">
-          <button onClick={retry}>다시 시작하기</button>
-        </div>
-      </div>
       </main>
       <style jsx>{`
         button {
           border: 1px solid black;
         }
         .imgAlign {
-            display: flex;
-            justify-content: center;
-            mix-blend-mode: difference;
-            margin: 10px;
+          display: flex;
+          justify-content: center;
+          mix-blend-mode: difference;
+          margin: -16px 0 -64px;
+        }
+        .subTitle {
+          display: flex;
+          justify-content: center;
+          mix-blend-mode: difference;
+          margin-bottom: 64px;
         }
         .p1 {
           font-weight: 500;
           font-size: 30px;
           line-height: 36px;
-          margin-top: 30px;
+          margin-top: 64px;
           text-align: center;
         }
         .p2 {
@@ -83,31 +98,31 @@ const Score: NextPage = () => {
           font-size: 25px;
           line-height: 30px;
           text-align: center;
-          margin-bottom: 30px;
+          margin-bottom: 32px;
         }
         button {
-            -webkit-appearance: none;
-            -moz-appearance: none;
-            appearance: none;
+          -webkit-appearance: none;
+          -moz-appearance: none;
+          appearance: none;
 
-            font-weight: 700;
-            font-size: 30px;
-            line-height: 38px;
+          font-weight: 700;
+          font-size: 30px;
+          line-height: 38px;
 
-            background: #000000;
-            color: #ffffff;
-            padding: 25px 140px;
-            display: inline-block;
-            width: auto;
-            border: none;
-            border-radius: 10px;
-            cursor: pointer;
-            transition: 0.5s;
+          background: #000000;
+          color: #ffffff;
+          padding: 25px 140px;
+          display: inline-block;
+          width: auto;
+          border: none;
+          border-radius: 10px;
+          cursor: pointer;
+          transition: 0.5s;
         }
         .bottonWrapper {
-            color: black;
-            justify-content: center;
-            text-align: center;
+          color: black;
+          justify-content: center;
+          text-align: center;
         }
       `}</style>
     </>
