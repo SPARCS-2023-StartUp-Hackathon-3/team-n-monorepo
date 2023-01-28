f4ea6a6d9a05a553b798fafc183205b178ae76b3
jyp930 <jyp930@gmail.com>
2023-01-28 14:51:29 +0900

    tailwind 로직 제거

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,4 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(request);
   if (request.contentScriptQuery === "/image/alt") {
     fetch("https://team-n-web.vercel.app/api/image/alt", {
       method: "POST",
