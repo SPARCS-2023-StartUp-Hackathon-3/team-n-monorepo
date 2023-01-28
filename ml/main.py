3daf5e8cedaf0972fcb88291bdf363f9814fe478
Jaechan <dlwocks31@gmail.com>
2023-01-28 11:06:41 +0900

    ML 서버 - 다른 모델 추가 예시 작성

--- ml/main.py
+++ ml/main.py
@@ -1,6 +1,8 @@
+import requests
 from fastapi import FastAPI
+from PIL import Image
 from pydantic import BaseModel
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoProcessor, pipeline
 
 app = FastAPI()
 
@@ -25,3 +27,26 @@ def generate_alt_text(item: RequestItem) -> ResponseItem:
 
     result = list(map(lambda a: a[0]['generated_text'], image_to_text(item.urls)))
     return {"result": result}
+
+@app.post("/multiple")
+def generate_multiple_model(item: RequestItem):
+    print(f'received url = {item.urls}')
+    if not item.urls:
+        return {"result": []}
+    
+    return {"result": [], "result2": generate_git_large_coco(item.urls)}
+
+
+def generate_git_large_coco(urls: list[str]) -> list[str]:
+    processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
+    model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco")
+
+    ls = []
+    for url in urls:
+        image = Image.open(requests.get(url, stream=True).raw)
+        pixel_values = processor(images=image, return_tensors="pt").pixel_values
+        generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
+        generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        ls.append(generated_caption)
+    
+    return ls
