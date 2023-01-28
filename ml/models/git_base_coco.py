dd07a86da8c75722e099fe428c4df338183375a1
Jaechan <dlwocks31@gmail.com>
2023-01-28 15:05:04 +0900

    cuda 관련 서버 최적화
    기존 약 3.7초 걸리던 엔드포인트

--- ml/models/git_base_coco.py
+++ ml/models/git_base_coco.py
@@ -1,9 +1,13 @@
 import requests
+import torch
 from PIL import Image
 from transformers import AutoModelForCausalLM, AutoProcessor
 
 processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
 model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco")
+
+if torch.cuda.is_available():
+    model = model.to('cuda')
 
 def git_base_coco(urls: list[str]) -> list[str]:
     images = [Image.open(requests.get(url, stream=True).raw) for url in urls]
@@ -11,6 +15,8 @@ def git_base_coco(urls: list[str]) -> list[str]:
     ls = []
     for image in images:
         pixel_values = processor(images=[image], return_tensors="pt").pixel_values
+        if torch.cuda.is_available():
+            pixel_values = pixel_values.to('cuda')
         generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
         generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
         ls.append(generated_caption)
