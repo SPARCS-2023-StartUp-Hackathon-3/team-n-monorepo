1c9ed26ca019685320988d50b00725f95ffcbdac
Jaechan <dlwocks31@gmail.com>
2023-01-28 17:16:34 +0900

    Revert "불필요한 inference 객체 삭제"
    This reverts commit 721079bbca48174dd38706f5e14727ade4d149d9.

--- web/src/server/util/InferenceService.ts
+++ web/src/server/util/InferenceService.ts
@@ -8,15 +8,33 @@ const prisma = new PrismaClient({
 
 export class InferenceService {
   private instance = axios.create({
-    baseURL: "http://noon.hackathon.sparcs.org",
+    baseURL: "http://127.0.0.1:8000",
   });
 
-  async getAlt(urls: string[]): Promise<{ url: string; result: string }[]> {
+  async getFromCacheOrPython(
+    urls: string[]
+  ): Promise<{ url: string; result: string }[]> {
     console.log("inference called");
+    // check existing alt
+    const cachedResult = await prisma.inference.findMany({
+      where: {
+        url: {
+          in: urls,
+        },
+      },
+    });
+
+    console.log("cachedResult", cachedResult);
+
+    const cachedUrls = cachedResult.map((r) => r.url);
+
+    const uncachedUrls = urls.filter((u) => !cachedUrls.includes(u));
+
+    console.log("uncachedUrls", uncachedUrls);
 
     const rawResponse = await this.instance
       .post<unknown>("/alt", {
-        urls,
+        urls: uncachedUrls,
       })
       .then((res) => res.data);
 
@@ -26,10 +44,18 @@ export class InferenceService {
       })
       .parse(rawResponse);
 
-    return urls.map((url, i) => {
-      return {
+    await prisma.inference.createMany({
+      data: uncachedUrls.map((url, i) => ({
         url,
         result: result[i] || "",
+      })),
+    });
+
+    return urls.map((url) => {
+      const cached = cachedResult.find((r) => r.url === url);
+      return {
+        url,
+        result: cached?.result || result[uncachedUrls.indexOf(url)] || "",
       };
     });
   }
