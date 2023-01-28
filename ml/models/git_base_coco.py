2c494d5d9a079a5e8765cc4ca0be3d75acb01548
Jaechan <dlwocks31@gmail.com>
2023-01-28 14:54:37 +0900

    ML 서버 - 모델 추가

--- ml/models/git_base_coco.py
+++ ml/models/git_base_coco.py
@@ -6,10 +6,11 @@ processor = AutoProcessor.from_pretrained("microsoft/git-base-coco")
 model = AutoModelForCausalLM.from_pretrained("microsoft/git-base-coco")
 
 def git_base_coco(urls: list[str]) -> list[str]:
+    images = [Image.open(requests.get(url, stream=True).raw) for url in urls]
+    
     ls = []
-    for url in urls:
-        image = Image.open(requests.get(url, stream=True).raw)
-        pixel_values = processor(images=image, return_tensors="pt").pixel_values
+    for image in images:
+        pixel_values = processor(images=[image], return_tensors="pt").pixel_values
         generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
         generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
         ls.append(generated_caption)
